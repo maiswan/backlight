222fe8b0ea091d0f172fab108ac11f60ea2ecc05
maiswan <173935047+maiswan@users.noreply.github.com>
2025-06-14 16:33:10 +0800

    Move dashboard to its own repo at maiswan/backlight-dashboard

--- dash/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import CustomCommand from "./CustomCommand";
-import QuickCommands from "./QuickCommands";
-import ServerInput from "./ServerInput";
-import ServerStatus from "./ServerStatus";
-
-export { ServerInput, ServerStatus, QuickCommands, CustomCommand }
