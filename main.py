448c049863dba3bb41f18537b11ae66862c20978
maiswan <173935047+maiswan@users.noreply.github.com>
2025-06-14 20:26:18 +0800

    Support private config

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
 # Singleton config
-import asyncio
-import json
-
-from fastapi.staticfiles import StaticFiles
 from model.state import state
 
 # HTTP server
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from routes.state_routes import router as state_router
 from routes.instruction_routes import router as instruction_router
 
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    render_task = asyncio.create_task(state.render_loop())
-    state.pixels.brightness = 1.0
-        
-    yield 
     # actions on exit
-    state.pixels.brightness = 0.0
-    state.pixels.show()
-    
-    with open('config.json', 'w') as f:
-        json.dump(state.config.to_dict(), f, indent=4)
-
-    state.stop_event.set()
-    await render_task
-
+    yield
+    await state.deconstruct()
 
 app = FastAPI(
     title="Backlight HTTP endpoint",
