3310da2650503dc027c44cab6873c41ec3fcfb8d
maiswan <173935047+maiswan@users.noreply.github.com>
2025-12-09 09:37:45 +0800

    Remove CORS

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from model.state import State
 # HTTP server
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from routes.config_routes import router as config_router
 from routes.command_routes import router as command_router
@@ -23,14 +22,6 @@ app = FastAPI(
     title="Backlight HTTP endpoint",
     description="Control WS2812B LED strip",
     lifespan=lifespan,
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["GET", "PUT", "POST", "DELETE"],
-    allow_headers=["*"],
 )
 
 app.include_router(config_router, prefix="/api/v3/config", tags=["config"])
