75e44331747c643d576015870cb150b85fee8179
maiswan <173935047+maiswan@users.noreply.github.com>
2026-03-31 13:22:06 +0800

    Add POST/DELETE endpoints

--- routes/server_routes.py
+++ routes/server_routes.py
@@ -1,46 +1,70 @@
 from fastapi import APIRouter, Body, status, Request, HTTPException
 from pydantic import ValidationError
 from model.state import State
-from .payloads import IntPayload, ListStrPayload
+from .payloads import IntPayload, StrPayload, ListStrPayload
 
 router = APIRouter()
 
 # port
 @router.get("/port")
 async def get_port(request: Request):
-    state = request.state.state
-    return state.config.server.port
+    config = request.state.state.config
+    return config.server.port
 
 @router.put("/port", status_code=status.HTTP_204_NO_CONTENT)
 async def put_led_count(request: Request, payload: IntPayload = Body(...)):
-    state = request.state.state
-    state.config.server.port = payload.value
-    state.config.write()
+    config = request.state.state.config
+    config.server.port = payload.value
+    config.write()
 
 
 
 # whitelist
 @router.get("/whitelist")
 async def get_whitelist(request: Request):
-    state = request.state.state
-    return state.config.server.whitelist
+    config = request.state.state.config
+    return config.server.whitelist
 
 @router.put("/whitelist", status_code=status.HTTP_204_NO_CONTENT)
 async def put_whitelist(request: Request, payload: ListStrPayload = Body(...)):
-    state = request.state.state
-    state.config.server.whitelist = payload.value
-    state.config.write()
+    config = request.state.state.config
+    config.server.whitelist = payload.value
+    config.write()
+
+@router.post("/whitelist", status_code=status.HTTP_204_NO_CONTENT)
+async def post_whitelist(request: Request, payload: StrPayload = Body(...)):
+    config = request.state.state.config
+    config.server.whitelist.append(payload.value)
+    config.write()
+
+@router.delete("/whitelist/{ip}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_whitelist(request: Request, ip: str):
+    config = request.state.state.config
+    config.server.whitelist = [ x for x in config.server.whitelist if not x == ip ]
+    config.write()
 
 
 
 # blacklist
 @router.get("/blacklist")
 async def get_blacklist(request: Request):
-    state = request.state.state
-    return state.config.server.blacklist
+    config = request.state.state.config
+    return config.server.blacklist
 
 @router.put("/blacklist", status_code=status.HTTP_204_NO_CONTENT)
 async def put_blacklist(request: Request, payload: ListStrPayload = Body(...)):
-    state = request.state.state
-    state.config.server.blacklist = payload.value
-    state.config.write()
+    config = request.state.state.config
+    config.server.blacklist = payload.value
+    config.write()
+
+@router.post("/blacklist", status_code=status.HTTP_204_NO_CONTENT)
+async def post_blacklist(request: Request, payload: StrPayload = Body(...)):
+    config = request.state.state.config
+    config.server.blacklist.append(payload.value)
+    config.write()
+
+@router.delete("/blacklist/{ip}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_blacklist(request: Request, ip: str):
+    config = request.state.state.config
+    config.server.blacklist = [ x for x in config.server.blacklist if not x == ip ]
+    config.write()
